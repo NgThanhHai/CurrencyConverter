d97e543e23957df0a063f78a7b3325bd1f219405
hainguyen <haigom00@gmail.com>
2021-10-17 14:18:45 +0700

    Add project files.

---
+++ CurrencyConverter/Controllers/ConverterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Controllers
+{
+    public class ConverterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("ConverterView" );
+        }
+        public IActionResult convert(int usd, int rate) {
+            Models.Currency curr = new Models.Currency(rate);
+            curr.convert(usd);
+            return View("ConverterView", curr);
+        }
+    }
+}
---
+++ CurrencyConverter/Models/Currency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CurrencyConverter.Models
+{
+    public class Currency
+    {
+        public int rate;
+        public object result;
+
+        public Currency(int rate)
+        {
+            this.rate = rate;
+        }
+
+        public void convert(int usd) {
+
+            result = rate * usd;
+        }
+    }
+}
